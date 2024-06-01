a156284ca78974d74a8360821a2ab2c3c6b0df89
Kayiira Richard W <richardwilsonevarist@gmail.com>
2024-06-01 20:57:46 +0300

    small

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6llpp&hqrnzvf$qhil_2w&y(pys9dw-!$7q6+2sa#ra6c!fsj1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
